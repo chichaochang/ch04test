0f850a0c56db5709ea9e5ddc9f1e61eb237735bb
chichaochang <chichao.chang@gmail.com>
2021-02-28 20:23:32 +0800

    Chapter 04 Final

--- mysite/views.py
+++ mysite/views.py
@@ -4,12 +4,16 @@ from mysite.models import Product
 import random
 
 
-def about(request):
+def index(request):
     quotes = ['今日事，今日畢',
             '要怎麼收穫，先那麼栽',
             '知識就是力量',
             '一個人的個性就是他的命運']
     quote = random.choice(quotes)
+    return render(request, 'index.html', locals())
+
+
+def about(request):
     return render(request, 'about.html', locals())
 
 
